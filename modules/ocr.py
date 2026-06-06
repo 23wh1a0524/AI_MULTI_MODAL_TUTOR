0369be7377be53c2e561f69b2f841ff01e3580cf
23wh1a0504 <150162173+23wh1a0504@users.noreply.github.com>
2026-06-06 19:11:03 +0530

    Update ocr.py

--- modules/ocr.py
+++ modules/ocr.py
@@ -2,7 +2,6 @@ import pytesseract
 from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
 def extract_text_from_image(image_path):
     try:
         img = Image.open(image_path)
